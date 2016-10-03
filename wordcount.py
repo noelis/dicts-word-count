265e21277f4d87fb58c7aca31c8922e8921ee714
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 12:07:59 -0700

    Pseudocode guide and starting word_count function

---
+++ wordcount.py
@@ -0,0 +1,19 @@
+def word_count(file_name):
+    """
+    Takes file, prints out number of times you see the same words
+    """
+    # open file
+    file_name_open = open(file_name)
+    # create empty dictionary 
+    word_count_dictionary = {}
+    # split the line, which returns a list of words
+    list_of_words = file_name_open.split(" ")
+# close file
+
+# store list of words in a variable
+
+# loop through list of words(use .get method)
+
+
+
+# print word count (maybe for loop needed?)
